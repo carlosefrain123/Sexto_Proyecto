91f379712f1c3430f577daf3091a6abb6ae788f8
carlosefrain123 <carlosefrainchallo1@gmail.com>
2023-12-02 16:39:33 -0500

    2/12/2023 - 16:38 - Sea rega completamente el template y el data table

---
+++ view/MntProductos/leftPanel.php
@@ -0,0 +1,25 @@
+<!-- ########## START: LEFT PANEL ########## -->
+<div class="br-logo"><a href=""><span>[</span>Limpio Be<span>]</span></a></div>
+<div class="br-sideleft overflow-y-auto">
+    <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
+    <div class="br-sideleft-menu">
+        <a href="../Home/index.php" class="br-menu-link">
+            <div class="br-menu-item">
+                <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+                <span class="menu-item-label">Dashboard</span>
+            </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+        <a href="#" class="br-menu-link">
+            <div class="br-menu-item">
+                <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+                <span class="menu-item-label">Mantenimiento</span>
+                <i class="menu-item-arrow fa fa-angle-down"></i>
+            </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+        <ul class="br-menu-sub nav flex-column">
+            <li class="nav-item"><a href="../MntProductos/" class="nav-link">Producto</a></li>
+        </ul>
+    </div><!-- br-sideleft-menu -->
+    <br>
+</div><!-- br-sideleft -->
+<!-- ########## END: LEFT PANEL ########## -->
